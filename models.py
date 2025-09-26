06686fbf6d8415ceee4905ade6748a6cdb725ee9
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-09-25 21:24:22 -0300

    adiciona modelo de Role e a relação de chave estrangeira com o modelo User

--- models.py
+++ models.py
@@ -12,6 +12,19 @@ def _ts():
     # helper para ter um serializer consistente
     return URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
 
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), unique= True, nullable = False )
+
+    can_moderate = db.Column(db.Boolean, default = False)
+    can_admin = db.Column(db.Boolean, default = False)
+
+    users = db.relationship('User', backref='role', lazy=True)
+
+    def __repr__(self):
+        return f"Role('{self.name}', Mod:{self.can_moderate}, Admin:{self.can_admin})"
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -20,6 +33,11 @@ class User(db.Model, UserMixin):
     role = db.Column(db.String(20), default='user', nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
     is_active = db.Column(db.Boolean, default=False)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False, default=1) 
+    
+    posts = db.relationship('Post', backref='author', lazy=True)
+
 
     def get_confirmation_token(self, expires_sec=1800):
         return _ts().dumps({'user_id': self.id}, salt='confirm-salt')
@@ -44,3 +62,12 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+#Funções auxiliares para Permissão
+
+def is_moderator(user):
+    return user.is_authenticated and user.role.can_moderate if hasattr(user, 'role') else False
+
+def is_admin(user):
+    return user.is_authenticated and user.role.can_admin if hasattr(user, 'role') else False
