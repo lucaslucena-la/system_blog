1a285885a2a8a5d814d4a083baafcd926954562c
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-09-25 21:18:02 -0300

    logica de inicialização dos papéis(padrão, moderador, admin)

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bcrypt import Bcrypt   
 import os
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 
-# === Config ===
-# Use uma SECRET_KEY fixa (ideal via variável de ambiente)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-change-me')
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'      # 'smtp.googlemail.com' também funciona
@@ -23,15 +21,38 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 mail = Mail(app)
 bcrypt = Bcrypt(app) 
+s = URLSafeTimedSerializer(app.config['SECRET_KEY']) 
 
 login_manager.login_view = 'login'
+login_manager.login_message_category = 'info' 
+login_manager.login_view = 'login'
 
-import views
+
+from models import User
+def load_user(user_id):
+    """Carrega um objeto User do DB a partir do user_id armazenado na sessão."""
+    # Usa db.session.get para busca direta por chave primária
+    try:
+        return db.session.get(User, int(user_id))
+    except ValueError:
+        return None
+
 #print("ROTAS CARREGADAS:", app.url_map)
+import views
 
 
 if __name__ == '__main__':
     with app.app_context():
+        from models import Role
         db.create_all()
-    # Evita rodar a thread de citação 2x no reloader do Flask
+
+        if Role.query.count() == 0:
+            print ("Inicializando Ppéis de Uduário")
+            default_role = Role(id=1, name='Padrão', can_moderate=False, can_admin=False)
+            moderador_role = Role(id=2, name='Moderador', can_moderate=False, can_admin=False)
+            adm_role = Role(id=3, name='Moderador', can_moderate=False, can_admin=True)
+            db.session.add_all([default_role, moderador_role, adm_role])
+            db.session.commit()
+            print("Papéis de usuário (Padrão, Moderador, Administrador) criados no DB.")
+
     app.run(debug=True, use_reloader=False)
